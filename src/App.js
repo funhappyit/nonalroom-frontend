5a46a33b799305ae8471d9351623f6c3fe8dae35
“funhappyit” <fun.happyit@gmail.com>
2025-06-03 16:25:47 +0900

    2025-06-03 한유경 create 작업 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import './App.css';
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
+import {useRef, useState} from "react";
+
+const mockTodo = [
+    {
+        id:0,
+        isDone: false,
+        content: "React 공부하기",
+        createDate: new Date().getTime(),
+    },
+    {
+        id:1,
+        isDone: false,
+        content: "빨래 넣기",
+        createDate: new Date().getTime(),
+    },
+    {
+        id:2,
+        isDone: false,
+        content: "노래 연습하기",
+        createDate: new Date().getTime(),
+    },
+]
 
 function App() {
-  return (
+    const [todo, setTodo] = useState(mockTodo);
+    const idRef = useRef(3);
+    const onCreate = (content) => {
+      const newItem = {
+        id:idRef.current,
+        content,
+        isDone:false,
+        createDate: new Date().getTime(),
+      };
+      setTodo([newItem,...todo]);
+      idRef.current +=1;
+    };
+
+    return (
       <div className="App">
         <Header/>
-        <TodoEditor/>
+        <TodoEditor onCreate={onCreate}/>
         <TodoList/>
       </div>
   );
